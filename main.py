7fd349cbce37863fec6a50c1cbdb1164b5b55f5f
SigeRios <happy.abdelilah@gmail.com>
2024-06-12 16:43:50 +0100

    Update to use environment variables

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import os
 import boto3
 from fastapi import FastAPI
 
 app = FastAPI()
 
-s3 = boto3.client('s3')
-BUCKET_NAME = 'abdelilahbucket'
+# Retrieve environment variables
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+# Initialize S3 client
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
 
 @app.get("/")
 def read_root():

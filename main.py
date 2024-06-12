1cbcaf5c516b933b966feeb476ed974b492cddc4
SigeRios <happy.abdelilah@gmail.com>
2024-06-12 16:09:56 +0100

    Set up AWS S3 integration

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import boto3
 from fastapi import FastAPI
 
 app = FastAPI()
 
+s3 = boto3.client('s3')
+BUCKET_NAME = 'abdelilahbucket'
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.post("/upload")
+def upload_data(file_name: str, data: str):
+    s3.put_object(Bucket=BUCKET_NAME, Key=file_name, Body=data)
+    return {"message": "Data uploaded"}
